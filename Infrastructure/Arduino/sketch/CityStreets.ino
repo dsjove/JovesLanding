45acf0527da56ea433dcb3f6f0b8980f29657568
David Giovannini <dsjove@gmail.com>
2025-04-26 08:45:20 -0500

    bug fix

--- Infrastructure/Arduino/sketch/CityStreets.ino
+++ Infrastructure/Arduino/sketch/CityStreets.ino
@@ -3,7 +3,7 @@
 CityStreets::CityStreets()
 : _ble("City Streets")
 , _matrixR4(_ble)
-, _lighting({{3, true}, {0, false}}, 3, _ble)
+, _lighting({{3, true}, {0, false}}, A0, _ble)
 , _servoMotor(9, _ble)
 {
 }
